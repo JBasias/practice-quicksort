9c3b9b44303df9de5f4a5e44e3ff4fcd92abca85
JBasias <jbasias@gmail.com>
2023-12-12 18:29:48 -0500

    a needed commit

--- quicksort.js
+++ quicksort.js
@@ -1,4 +1,31 @@
-function quicksort(arr) {
+
+//function Pivot()
+
+
+function quicksort(A) {
+
+  //console.log(A);
+
+  if(A.length <=1) return A;
+
+
+  let piv = A[0]; let L=[]; let R=[];
+
+
+  for(let i=1;i<A.length ;i++)
+  {
+
+    if(A[i]<piv) L.push(A[i]);
+    else R.push(A[i]);
+
+  }
+
+  //console.log(L + " -- " + piv + " ---" + R)
+
+  const sortedL = quicksort(L);
+  const sortedR = quicksort(R);
+
+  return sortedL.concat([piv], sortedR);
 
   // Check if the input is length 1 or less
     // If so, it's already sorted: return
@@ -15,6 +42,8 @@ function quicksort(arr) {
   // Return the left, pivot and right in sorted order
 
 }
+
+
 
 
 module.exports = [quicksort];
